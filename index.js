ece1f0cc4e43261d642a6a4ffbde83e4060369ce
arpit <arpitghr12@gmail.com>
2025-05-20 08:18:49 +0530

    Hello

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ const app=express();
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.get("/",(req,res)=>{
